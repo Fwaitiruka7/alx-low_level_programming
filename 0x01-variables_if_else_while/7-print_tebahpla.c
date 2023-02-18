f6011bf184a5da3fae81627ba87ed2d855a07eb9
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 05:38:38 +0300

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet_1;
+
+	for (alphabet_1 = 'z'; alphabet_1 <= 'a'; alphabet_1++)
+	{
+		putchar(alphabet_1);
+	}
+		putchar('\n');
+
+		return (0);
+}
