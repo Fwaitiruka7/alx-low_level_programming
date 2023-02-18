49207b2cef3616a2a433f33e187df7a508d43da7
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 04:39:25 +0300

    print alphabets with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+       	char PP1;
+	
+	/* for loop to create lowercase alphabests */
+	for (PP1 = 'a'; PP1 <= 'z'; PP1++)
+	{
+		if ( PP1 != 'q' && PP1 != 'e')
+			putchar (PP1);
+	}
+	putchar ('\n');
+	return (0);
+}
