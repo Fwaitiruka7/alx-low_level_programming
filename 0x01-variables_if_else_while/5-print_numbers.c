efeffde1185e07ca3c830e41709169f02135b828
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 05:00:02 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number_x;
+
+	/* for loop statement for  single digit decrement */
+
+	for (number_x = 0; number_x < 10; number_x++)
+		printf("%d", number_x);
+
+	printf("\n");
+
+	return (0);
+}
