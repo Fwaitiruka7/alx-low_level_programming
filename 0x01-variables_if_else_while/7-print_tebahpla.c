ccfe961756b1acff2a8e5af6a3948210ded64b3b
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 05:51:56 +0300

    reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char alphabet_1;
 
-	for (alphabet_1 = 'z'; alphabet_1 <= 'a'; alphabet_1--)
+	for (alphabet_1 = 'z'; alphabet_1 >= 'a'; alphabet_1--)
 	{
 		putchar(alphabet_1);
 	}
