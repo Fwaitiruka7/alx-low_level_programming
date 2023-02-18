70b23076bf53a681e7205e36eb3b5fe6594e2f5b
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 06:00:56 +0300

    spaces'

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number_x;
+	for (number_x = 0; number_x < 10; number_x++)
+		putchar((number_x % 10) + '0');
+		if (number_x == 9)
+			continue;
+		putchar (',');
+		putchar (' ');
+
+	putchar('\n');
+
+	return (0);
+}
