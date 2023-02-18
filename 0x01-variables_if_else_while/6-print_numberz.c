bbc8b958c80e5b28f95aa3a4a6afccf4e1686518
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-18 05:22:37 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number_x;
+	
+	/* for loop statement for  single digit increment */
+
+	for (number_x = 0; number_x < 10; number_x++)
+		putchar((number_x % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
