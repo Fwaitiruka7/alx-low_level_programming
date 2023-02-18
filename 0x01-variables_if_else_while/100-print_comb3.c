b9f3308c1a2c4d7d0fcacab62fb33de17ff5f50b
francis waitiruka <fanciswaitiruka777@gmail.com>
2023-02-18 14:05:06 -0800

    combination3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number_x;
+
+	for (number_x = 0; number_x < 100; number_x++)
+	{
+		putchar((number_x / 10) + '0');
+		putchar((number_x % 10) + '0');
+		if (number_x == 90)
+			continue;
+		putchar (',');
+		putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
