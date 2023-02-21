6c2c82da5dd80cb5dbfb412f8a864029fb21d9af
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-21 23:20:30 +0300

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
+ * main - Entry point
  *
- * Return: void
- */
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
 
 void print_alphabet_x10(void)
 {
 	char letter;
 	int i = 0;
-	
+
 	while (i <= 10)
 	{
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	
 	i++;
+	_putchar('\n');
 	}
 
-	_putchar('\n');
 }
