24de24715c3e3012ae74e99ac59044860ed0cc19
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-21 21:17:15 +0300

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char L;
+
+	for (L = 'a'; L <= 'z'; L++)
+	{
+		_putchar('L');
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 /*prototypes functions */
 void _putchar();
+void print_alphabet(void);
