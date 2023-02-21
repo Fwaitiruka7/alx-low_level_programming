285d321b474e2683b63f87420b223cb35350841b
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-22 02:50:45 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs - computes  the absolute value of an integer.
+ *int_abs: prototype function for absolute
+ * @int: the integer assigned value
+ * Return: Always (0)
+ */
+
+int _abs(int)
+{
+	int r;
+	int x;
+
+	x = _abs(r);
+	_putchar(r);
+
+	return (0);
