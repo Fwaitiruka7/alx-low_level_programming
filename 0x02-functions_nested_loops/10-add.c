da8602853237aca9911207f6881156ea55298677
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-22 04:58:24 +0300

    sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - sums up two integers
+ *@a: number value one
+ *@b: number value two
+ *Return: 0
+ */
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	_putchar(x);
+	return (0);
+}
