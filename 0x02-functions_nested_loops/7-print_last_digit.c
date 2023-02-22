ce0bc5e831564a07ddab65f55e79023d61521172
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-22 04:06:46 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit - returns the value of the last digit
+ *@x: value to be declared
+ *Return: 0
+ */
+
+int print_last_digit(int x)
+{
+	_putchar(x % 10);
+	_putchar('\n');
+	return (0);
+}
