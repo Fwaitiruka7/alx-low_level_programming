2ab85b2126bf45eef2d3b910c47e9db1168b4251
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-23 16:56:28 +0300

    multiplication is key

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function for multiplying
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
