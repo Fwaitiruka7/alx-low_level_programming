da6da6c7b4386bf169990bfcbbf75168f2e5a5cd
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-24 01:20:19 +0300

    squares is void

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int r, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 1; c <= size; c++)
+		{
+			_putchar('#');
+			
+			for (r = 2; r <= size; r++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+}
