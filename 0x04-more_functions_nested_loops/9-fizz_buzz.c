05edf75d79f0f71345c9068049bc2b0767643c9a
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-24 02:59:48 +0300

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,11 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if (x == 100)
+		{
+			printf("%s", b);
+		}
+		else if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("%s ", c);
 		}
