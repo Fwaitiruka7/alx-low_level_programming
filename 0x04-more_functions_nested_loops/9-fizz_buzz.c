a289ac7770e5a04d8174e287829232b2c840b812
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-24 02:50:19 +0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	char *a = "Fizz";
+	char *b = "Buzz";
+	char *c = "FizzBuzz";
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("%s ", c);
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s ", a);
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s ", b);
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
