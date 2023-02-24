e2c0bd15b7f84ae977172701929934b28eb3fd5a
Francis Waitiruka <root@FRANCIS-MW.localdomain>
2023-02-24 03:21:19 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a square
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int r, c, tri;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r <= (size - 1); r++)
+		{
+			for (my_sp = 0; my_sp = (size - 1) - r; r++)
+			{
+				_putchar(' ');
+			}
+			for (tri = 1; tr <= r; tr++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+}
