c34eb768b5c76bcd5c275118b4fed43e8b06c5a8
francis <franciswaitiruka777@gmail.com>
2023-03-01 02:47:54 +0300

    my first array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array to be used
+ * @n: number of elements of the array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	*a[5] = {98, 402, -198, 298, -1024};
+
+	for (n = 0; n < 5; n++)
+		printf("%d", array);
+}
