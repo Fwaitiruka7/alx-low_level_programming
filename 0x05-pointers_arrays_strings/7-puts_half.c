db7c4eba8afd6265af5dfd1fe773b92278743f31
francis <franciswaitiruka777@gmail.com>
2023-03-01 02:08:04 +0300

    my better half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ * @str: variables to be printed
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int length_of_the_string = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length_of_the_string++;
+
+		n = (length_of_the_string / 2);
+	}
+
+	if ((n % 2) != 0)
+	{
+		n = (length_of_the_string + 1) / 2;
+
+		for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
