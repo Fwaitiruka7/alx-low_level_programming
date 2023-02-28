e045fd9364f5560c2362a203062b0a3bc7214065
francis <franciswaitiruka777@gmail.com>
2023-02-28 22:17:15 +0300

    trial reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: character to be used
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+	int c = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		_putchar(s[a]);
+		_putchar('\n');
+		c++;
+	}
+
+	for (b = (c - 1); b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
