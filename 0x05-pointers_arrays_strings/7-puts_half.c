0ee4b68db9e2ef4cdc95b99a4df17b5157a6c22a
francis <franciswaitiruka777@gmail.com>
2023-03-01 02:25:43 +0300

    my half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,13 +18,15 @@ void puts_half(char *str)
 
 		n = (length_of_the_string / 2);
 	}
-	
+
 	if ((n % 2) != 0)
 	{
 		n = (length_of_the_string + 1) / 2;
-		
-		for (i = n; str[i] != '\0'; i++)
-			_putchar(str[i]);
+	}
+	
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
