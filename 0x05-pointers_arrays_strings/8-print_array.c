9be2b6998caa0fb2566377d884fb6454aa58d3cb
francis <franciswaitiruka777@gmail.com>
2023-03-01 03:01:24 +0300

    my trial array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,8 +9,8 @@
 
 void print_array(int *a, int n)
 {
-	a[5] = {98, 402, -198, 298, -1024};
-
-	for (n = 0; n < 5; n++)
-		printf("%d ", *a);
+	int ria;
+	for (ria = 0; ria < ( n - 1); ria++)
+		printf("%d, ", a[i]);
+	printf("\n");
 }
