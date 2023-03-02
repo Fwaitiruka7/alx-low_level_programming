b709f47d8782a8ade33aecdb5f06af57e5ce9f5c
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-03 00:24:29 +0300

    capitilization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: string to be capitalized
+ * Return: the capitilized string
+ */
+
+char *cap_string(char *str)
+{
+	int o;
+
+	for (o = 0; str[o] != '\0'; o++)
+	{
+		if ((o == 0 && str[o] != '\0') || (str[o] != ' ' && str[o - 1] == ' '))
+				{
+			if (str[o] >= 'a' && str[o] <= 'z')
+			{
+				str[o] = (int)str[o] - 32;
+			}
+		}
+	}
+	return (str);
+}
