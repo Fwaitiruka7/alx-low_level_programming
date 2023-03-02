0e7d26d92de4be175c0cc5bb7d0724c748f28253
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-03 02:20:05 +0300

    caps to sentence

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,22 +9,23 @@
 char *cap_string(char *str)
 {
 	int o, sc;
+	int capitals = 32;
 	int seps[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' '};
 
 	for (o = 0; str[o] != '\0'; o++)
 	{
-		if ((o == 0 && str[o] == '\0') || (str[o] != ' ' && str[o - 1] == ' '))
+		if (str[o] >= 'a' && str[o] <= 'z')
 		{
-			if (str[o] >= 'a' && str[o] <= 'z')
+			str[o] = (int)str[o] - capitals;
+		}
+		capitals = 0;
+
+		for (sc = 0; sc <= 12; sc++)
+		{
+			if (str[o] == seps[sc])
 			{
-				str[o] = (int)str[o] - 32;
-			}
-			for (sc = 0; sc <= 12; sc++)
-			{
-				if (str[o] == seps[sc])
-				{
-					sc = 12;
-				}
+				sc = 12;
+				capitals = 32;
 			}
 		}
 	}
