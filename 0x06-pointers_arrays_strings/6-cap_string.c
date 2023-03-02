f623905544a1e08e634542a62eefa43574dc33ca
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-03 00:50:33 +0300

    capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,23 @@
 
 char *cap_string(char *str)
 {
-	int o;
+	int o, sc;
+	int seps[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' '};
 
 	for (o = 0; str[o] != '\0'; o++)
 	{
 		if ((o == 0 && str[o] == '\0') || (str[o] != ' ' && str[o - 1] == ' '))
-				{
+		{
 			if (str[o] >= 'a' && str[o] <= 'z')
 			{
 				str[o] = (int)str[o] - 32;
+			}
+			for (sc = 0; sc <= 12; sc++)
+			{
+				if (str[o] == seps[sc])
+				{
+					sc = 12;
+				}
 			}
 		}
 	}
