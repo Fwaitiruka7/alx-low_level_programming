25e9941ac3d189843d8210813aba86474d9af68c
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-02 22:10:09 +0300

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int rio;
 
-	for  (rio = 0; s1[rio] != '\0' && s2[rio] != '\0'; rio ++)
+	for  (rio = 0; s1[rio] != '\0' && s2[rio] != '\0'; rio++)
 	{
 		if (s1[rio] != s2[rio])
 		{
-			return(s1[rio] - s2[rio]);
+			return (s1[rio] - s2[rio]);
 		}
 	}
 	return (0);
