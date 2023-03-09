45a0071b1fc7ca48ec9abc909e45babfe8824486
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-09 19:13:03 +0300

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function to check if its palindrome
+ * @s: character string to be checked
+ * Return: 1 if its palindrome and 0 if its not
+ */
+
+int is_palindrome(char *s)
+{
+	int sc = 0;
+	int ec = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		if (s[sc] == s[ec])
+		{
+			return (1);
+		}
+
+		else if (s[sc] != s[ec])
+		{
+			return (0);
+		}
+		else if (s[sc] < *s / 2)
+		{
+			sc++;
+			ec--;
+			is_palindrome(s);
+		}
+	}
+}
