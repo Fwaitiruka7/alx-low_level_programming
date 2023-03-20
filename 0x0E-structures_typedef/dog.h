24c3aeb2d4e9e4cfd662b9b8e694e6fa179b4aa2
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-21 01:52:25 +0300

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - used to create a new element of my_dog
+ *@name: contains the dog's name
+ *@age: details the dog's age
+ *@owner: shows the owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
