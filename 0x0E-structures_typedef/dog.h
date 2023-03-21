0694e40e76ed2ebc9550dfce235bb221574908a8
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-21 04:09:26 +0300

    new struct name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,8 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog_t - new name for struct dog
- * struct dog - used to create a elements for dog
+ *struct dog - used to create a elements for dog_t
  *@name: contains the dog's name
  *@age: details the dog's age
  *@owner: shows the owner's name
@@ -14,14 +13,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-} my_dog;
+} dog_t;
 
 /* funtion prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
