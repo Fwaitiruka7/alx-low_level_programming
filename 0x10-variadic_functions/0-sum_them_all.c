32010fc7392b38f8439a587deca04b6d6a6e2b4b
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-24 15:34:29 +0300

    param sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,9 +2,10 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum of all its parameters
- * @n: number of parameters
- * Returns: if n == 0 return 0, if not sum
+ * sum_them_all - returns the sum of all its parameters
+ * @n: numb of interger parameter to sum
+ * @...: interger parameter to sum
+ * Return: sum of all parameters and if n is equal to 0, return 0
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -15,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (n==0)
+	if (n == 0)
 	{
 		return (0);
 	}
