f24a1120ef55e4643d7c3942c3f0cdc1b2d9da63
francis waitiruka <franciswaitiruka777@gmail.com>
2023-03-31 14:11:26 +0300

    mr tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - prints text before the main func is exec
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
