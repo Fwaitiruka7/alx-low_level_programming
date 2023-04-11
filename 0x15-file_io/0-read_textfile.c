495fbe4b67eecd0f11c26c494cf71f8f0f060970
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-11 22:10:04 +0300

    read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads and prints text to POSIX stdout
+ * @filename: pointer variable
+ * @letters: number of letters
+ * Return: 0 if file can't be read/opened, if filename is NULL & can't write
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp = fopen(filename, "r");
+	char *buffer = (char *) malloc(sizeof(char) * letters);
+	size_t read_fl = fread(buffer, sizeof(char), letters, fp);
+	ssize_t write_fl = write(STDOUT_FILENO, buffer, read_fl);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	if (buffer == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	if (read_fl == 0 || ferror(fp))
+	{
+		free(buffer);
+		fclose(fp);
+		return (0);
+	}
+
+	if (write_fl == -1 || (unsigned long int) write_fl != (size_t) read_fl)
+	{
+		free(buffer);
+		fclose(fp);
+		return (0);
+	}
+
+	free(buffer);
+	fclose(fp);
+	return ((size_t) read_fl);
+}
