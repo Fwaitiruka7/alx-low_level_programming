375a2f12b76878491ce8cf00277613681300df9f
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-11 23:32:21 +0300

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	FILE *file = fopen(filename, "w+");
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (file == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		fputs(text_content, file);
+	}
+
+	fclose(file);
+	return (1);
 }
