3f2fb3a9f1a651e85f5844e5716f9d8ef7d90094
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-12 01:00:05 +0300

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,6 +9,7 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: file to be created or appended
  * @text_content: text content to be appended
+ * Return: 1 on success and -1 if fail, filename is NULL or no permission
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
