b9a3215e676ef065045c5ed46c95097103504ea4
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-30 02:27:56 +0300

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include "main.h"
+#include <sys/stat.h>
 
 /**
  * create_file - creates a file
@@ -14,28 +15,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
-	int count = strlen(text_content), str;
+	int fd;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	if (file == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 	{
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
-		str = write(file, text_content, count);
-		if (str == -1)
+		ssize_t len = strlen(text_content);
+		ssize_t written = write(fd, text_content, len);
+
+		if (written == -1)
 		{
+			close(fd);
 			return (-1);
 		}
 	}
-
-	close(file);
-	return (1);
+	close(fd);
+	return (-1);
 }
